8bb96561314434b73b17f0a05f5b932e5e2c9560
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2021-08-27 01:28:56 +0700

    init

---
+++ src/controllers/posts.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import PostMessage from "../models/postMessage"
+
+export const getPosts = async (req: express.Request, res: express.Response) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+
+export const createPost = async (req: express.Request, res: express.Response) => {
+  const post = req.body
+  const newPost = new PostMessage(post)
+  try {
+    await newPost.save();
+    res.status(201).json(newPost)
+  } catch (error) {
+    res.status(409).json({ message: error.message })
+  }
+}
